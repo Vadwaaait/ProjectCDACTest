a36cfd28099463d30c37b29f21ac73afca5f2a5c
Vadwaaait <gawade.mayuresh03@gmail.com>
2023-08-25 02:53:09 +0530

    25th aug

--- src/main/java/com/example/demo/Dto/BookingDTO.java
+++ src/main/java/com/example/demo/Dto/BookingDTO.java
@@ -1,5 +1,7 @@
 package com.example.demo.Dto;
 
+import java.sql.Date;
+
 import com.example.demo.Entity.HotelEntity;
 import com.example.demo.Entity.UserEntity;
 
@@ -10,16 +12,64 @@ public class BookingDTO {
 	private UserEntity userE;
 	
 	private HotelEntity hotelE;
-
-	public BookingDTO( UserEntity userE, HotelEntity hotelE) {
-		super();
 	
+private Date fromDate;
+	
+	private Date toDate;
+	
+	private int noOfDays;
+	
+	private double amountPaid;
+
+	
+
+	public BookingDTO(UserEntity userE, HotelEntity hotelE, Date fromDate, Date toDate, int noOfDays,
+			double amountPaid) {
+		super();
 		this.userE = userE;
 		this.hotelE = hotelE;
+		this.fromDate = fromDate;
+		this.toDate = toDate;
+		this.noOfDays = noOfDays;
+		this.amountPaid = amountPaid;
 	}
 
 	public int getBookingId() {
 		return bookingId;
+	}
+	
+	
+
+	public Date getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public Date getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+
+	public int getNoOfDays() {
+		return noOfDays;
+	}
+
+	public void setNoOfDays(int noOfDays) {
+		this.noOfDays = noOfDays;
+	}
+
+	public double getAmountPaid() {
+		return amountPaid;
+	}
+
+	public void setAmountPaid(double amountPaid) {
+		this.amountPaid = amountPaid;
 	}
 
 	public void setBookingId(int bookingId) {
@@ -44,7 +94,8 @@ public class BookingDTO {
 
 	@Override
 	public String toString() {
-		return "BookingDTO [bookingId=" + bookingId + ", userE=" + userE + ", hotelE=" + hotelE + "]";
+		return "BookingDTO [bookingId=" + bookingId + ", userE=" + userE + ", hotelE=" + hotelE + ", fromDate="
+				+ fromDate + ", toDate=" + toDate + ", noOfDays=" + noOfDays + ", amountPaid=" + amountPaid + "]";
 	}
 	
 	
